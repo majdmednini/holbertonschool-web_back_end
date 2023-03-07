8cdf383fc361f01d2d27550a3c349519a96ad236
threadsv1 <majdimed3@gmail.com>
2023-03-07 21:02:42 +0100

    PEP8

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -6,6 +6,62 @@ import redis
 import sys
 from typing import Callable, Optional, Union
 import uuid
+from curses import keyname
+from functools import wraps
+
+
+def replay(fn):
+    """
+    4. Retrieving lists
+    """
+    store = redis.Redis()
+    count_key = fn.__qualname__
+    input_key = count_key + ":inputs"
+    output_key = count_key + ":outputs"
+    count = store.get(count_key).decode("utf-8")
+    print("{} was called {} times:".format(count_key, count))
+    inputs = store.lrange(input_key, 0, count)
+    outputs = store.lrange(output_key, 0, count)
+    for input, output in zip(inputs, outputs):
+        input = input.decode("utf-8")
+        output = output.decode("utf-8")
+        print("{}(*{}) -> {}".format(count_key, input, output))
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    2. Incrementing values
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        mthd to increment the count
+        """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    3. Storing lists
+    """
+    key = method.__qualname__
+    input_key = key + ":inputs"
+    output_key = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        mthd to push history in store
+        """
+        self._redis.rpush(input_key, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(data))
+        return data
+    return wrapper
 
 
 class Cache():
