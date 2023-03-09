4630c4a07f04cfd2eaa0334543c97421d4fab906
threadsv1 <majdimed3@gmail.com>
2023-03-09 22:58:27 +0100

    cmon

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- 3. Old school band
-SELECT band_name, (IFNULL(split, 2020) -  formed) AS lifespan FROM metal_bands WHERE style REGEXP 'Glam rock' ORDER BY lifespan DESC;
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ;
