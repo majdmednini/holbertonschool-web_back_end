47fd15b564d8f0a1bebb282b719a543ae736f3c7
threadsv1 <majdimed3@gmail.com>
2023-03-09 22:59:14 +0100

    fixed

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- 3. Old school band
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ;
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands WHERE style LIKE '%Glam rock%';
